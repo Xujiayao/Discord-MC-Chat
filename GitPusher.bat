754d58bc7a795b54e7d81888d3ed6942b88121f0
Xujiayao <caiandxu@163.com>
2021-12-27 17:46:43 +0800

    删除GitPusher+更新Gradle

--- GitPusher.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title Git Pusher By Xujiayao
-cd "C:\Users\Xujiayao\Desktop\Java Projects\MCDiscordChat"
-git add .
-echo Enter commit message: 
-set /p commit_message=
-git commit -m "%commit_message%"
-git push origin master
-pause
