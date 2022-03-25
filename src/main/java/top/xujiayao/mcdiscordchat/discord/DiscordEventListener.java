4f7957ffa94438695a15108d33992804e576ccee
Xujiayao <xujiayao147258369@gmail.com>
2022-03-26 02:00:09 +0800

    移除多余的try&catch

--- src/main/java/top/xujiayao/mcdiscordchat/discord/DiscordEventListener.java
+++ src/main/java/top/xujiayao/mcdiscordchat/discord/DiscordEventListener.java
@@ -42,129 +42,124 @@ public class DiscordEventListener extends ListenerAdapter {
 		e.deferReply().queue();
 
 		if (!e.isFromGuild() || (e.getChannel() != CHANNEL)) {
-			e.getHook().sendMessage("**Error!**").queue();
+			e.getHook().sendMessage(CONFIG.generic.useEngInsteadOfChin ? "**You can only send this command on the correct channel!**" : "**你只能在正确的频道发送此命令！**").queue();
 			return;
 		}
 
-		try {
-			switch (e.getName()) {
-				case "info" -> {
-					StringBuilder message = new StringBuilder()
-							.append("```\n=============== ")
-							.append(CONFIG.generic.useEngInsteadOfChin ? "Server Status" : "运行状态")
-							.append(" ===============\n\n");
+		switch (e.getName()) {
+			case "info" -> {
+				StringBuilder message = new StringBuilder()
+						.append("```\n=============== ")
+						.append(CONFIG.generic.useEngInsteadOfChin ? "Server Status" : "运行状态")
+						.append(" ===============\n\n");
 
-					// Online players
-					List<ServerPlayerEntity> onlinePlayers = SERVER.getPlayerManager().getPlayerList();
-					message.append(CONFIG.generic.useEngInsteadOfChin ? "Online players (" : "在线玩家 (")
-							.append(onlinePlayers.size())
-							.append(")")
-							.append(CONFIG.generic.useEngInsteadOfChin ? ":" : "：")
-							.append("\n");
+				// Online players
+				List<ServerPlayerEntity> onlinePlayers = SERVER.getPlayerManager().getPlayerList();
+				message.append(CONFIG.generic.useEngInsteadOfChin ? "Online players (" : "在线玩家 (")
+						.append(onlinePlayers.size())
+						.append(")")
+						.append(CONFIG.generic.useEngInsteadOfChin ? ":" : "：")
+						.append("\n");
 
-					if (onlinePlayers.isEmpty()) {
-						message.append(CONFIG.generic.useEngInsteadOfChin ? "No players online!" : "当前没有在线玩家！");
-					} else {
-						for (ServerPlayerEntity player : onlinePlayers) {
-							message.append("[").append(player.pingMilliseconds).append("ms] ").append(player.getEntityName());
-						}
-					}
-
-					// Server TPS
-					double serverTickTime = MathHelper.average(SERVER.lastTickLengths) * 1.0E-6D;
-					message.append(CONFIG.generic.useEngInsteadOfChin ? "\n\nServer TPS:\n" : "\n\n服务器 TPS：\n")
-							.append(Math.min(1000.0 / serverTickTime, 20));
-
-					// Server MSPT
-					message.append(CONFIG.generic.useEngInsteadOfChin ? "\n\nServer MSPT:\n" : "\n\n服务器 MSPT：\n")
-							.append(serverTickTime);
-
-					// Server used memory
-					message.append(CONFIG.generic.useEngInsteadOfChin ? "\n\nServer used memory:\n" : "\n\n服务器已用内存：\n")
-							.append((Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1024 / 1024)
-							.append(" MB / ")
-							.append(Runtime.getRuntime().totalMemory() / 1024 / 1024)
-							.append(" MB");
-
-					message.append("\n```");
-					e.getHook().sendMessage(message.toString()).queue();
-				}
-				case "help" -> e.getHook().sendMessage(CONFIG.generic.useEngInsteadOfChin ? """
-						```
-						=============== Help ===============
-						/info              | Query server running status
-						/help              | Get a list of available commands
-						/update            | Check for update
-						/blacklist         | Query MCDiscordChat blacklist
-						/reload            | Reload MCDiscordChat config file (admin only)
-						/console <command> | Execute a command in the server console (admin only)
-						/stop              | Stop the server (admin only)
-						```""" : """
-						```
-						=============== 帮助 ===============
-						/info              | 查询服务器运行状态
-						/help              | 获取可用命令列表
-						/update            | 检查更新
-						/blacklist         | 查询 MCDiscordChat 黑名单
-						/reload            | 重新加载 MCDiscordChat 配置文件（仅限管理员）
-						/console <command> | 在服务器控制台中执行命令（仅限管理员）
-						/stop              | 停止服务器（仅限管理员）
-						```""").queue();
-				case "update" -> e.getHook().sendMessage(Utils.checkUpdate(true)).queue();
-				case "blacklist" -> e.getHook().sendMessage("**Not yet implemented!**").queue();
-				case "reload" -> {
-					if (CONFIG.generic.adminsIds.contains(Objects.requireNonNull(e.getMember()).getId())) {
-						try {
-							ConfigManager.init();
-
-							Utils.setBotActivity();
-
-							CHANNEL = JDA.getTextChannelById(CONFIG.generic.channelId);
-							if (!CONFIG.generic.consoleLogChannelId.isEmpty()) {
-								CONSOLE_LOG_CHANNEL = JDA.getTextChannelById(CONFIG.generic.consoleLogChannelId);
-							}
-
-							Utils.updateBotCommands();
-
-							e.getHook().sendMessage(CONFIG.generic.useEngInsteadOfChin ? "**Config file reloaded successfully!**" : "**配置文件重新加载成功！**").queue();
-						} catch (Exception ex) {
-							LOGGER.error(ExceptionUtils.getStackTrace(ex));
-							e.getHook().sendMessage(CONFIG.generic.useEngInsteadOfChin ? "**Config file reload failed!**" : "**配置文件重新加载失败！**").queue();
-						}
-					} else {
-						e.getHook().sendMessage(CONFIG.generic.useEngInsteadOfChin ? "**You do not have permission to use this command!**" : "**你没有权限使用此命令！**").queue();
+				if (onlinePlayers.isEmpty()) {
+					message.append(CONFIG.generic.useEngInsteadOfChin ? "No players online!" : "当前没有在线玩家！");
+				} else {
+					for (ServerPlayerEntity player : onlinePlayers) {
+						message.append("[").append(player.pingMilliseconds).append("ms] ").append(player.getEntityName());
 					}
 				}
-				case "console" -> {
-					if (CONFIG.generic.adminsIds.contains(Objects.requireNonNull(e.getMember()).getId())) {
-						String command = Objects.requireNonNull(e.getOption("command")).getAsString();
-						if (command.equals("stop") || command.equals("/stop")) {
-							e.getHook().sendMessage(CONFIG.generic.useEngInsteadOfChin ? "**Stopping the server!**" : "**正在停止服务器！**")
-									.submit()
-									.whenComplete((v, ex) -> SERVER.stop(true));
-						} else {
-							e.getHook().sendMessage(CONFIG.generic.useEngInsteadOfChin ? "**Executing the command!**" : "**正在执行命令！**")
-									.submit()
-									.whenComplete((v, ex) -> SERVER.getCommandManager()
-											.execute(SERVER.getCommandSource().withOutput(new DiscordCommandOutput()), command));
+
+				// Server TPS
+				double serverTickTime = MathHelper.average(SERVER.lastTickLengths) * 1.0E-6D;
+				message.append(CONFIG.generic.useEngInsteadOfChin ? "\n\nServer TPS:\n" : "\n\n服务器 TPS：\n")
+						.append(Math.min(1000.0 / serverTickTime, 20));
+
+				// Server MSPT
+				message.append(CONFIG.generic.useEngInsteadOfChin ? "\n\nServer MSPT:\n" : "\n\n服务器 MSPT：\n")
+						.append(serverTickTime);
+
+				// Server used memory
+				message.append(CONFIG.generic.useEngInsteadOfChin ? "\n\nServer used memory:\n" : "\n\n服务器已用内存：\n")
+						.append((Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1024 / 1024)
+						.append(" MB / ")
+						.append(Runtime.getRuntime().totalMemory() / 1024 / 1024)
+						.append(" MB");
+
+				message.append("\n```");
+				e.getHook().sendMessage(message.toString()).queue();
+			}
+			case "help" -> e.getHook().sendMessage(CONFIG.generic.useEngInsteadOfChin ? """
+					```
+					=============== Help ===============
+					/info              | Query server running status
+					/help              | Get a list of available commands
+					/update            | Check for update
+					/blacklist         | Query MCDiscordChat blacklist
+					/reload            | Reload MCDiscordChat config file (admin only)
+					/console <command> | Execute a command in the server console (admin only)
+					/stop              | Stop the server (admin only)
+					```""" : """
+					```
+					=============== 帮助 ===============
+					/info              | 查询服务器运行状态
+					/help              | 获取可用命令列表
+					/update            | 检查更新
+					/blacklist         | 查询 MCDiscordChat 黑名单
+					/reload            | 重新加载 MCDiscordChat 配置文件（仅限管理员）
+					/console <command> | 在服务器控制台中执行命令（仅限管理员）
+					/stop              | 停止服务器（仅限管理员）
+					```""").queue();
+			case "update" -> e.getHook().sendMessage(Utils.checkUpdate(true)).queue();
+			case "blacklist" -> e.getHook().sendMessage("**Not yet implemented!**").queue();
+			case "reload" -> {
+				if (CONFIG.generic.adminsIds.contains(Objects.requireNonNull(e.getMember()).getId())) {
+					try {
+						ConfigManager.init();
+
+						Utils.setBotActivity();
+
+						CHANNEL = JDA.getTextChannelById(CONFIG.generic.channelId);
+						if (!CONFIG.generic.consoleLogChannelId.isEmpty()) {
+							CONSOLE_LOG_CHANNEL = JDA.getTextChannelById(CONFIG.generic.consoleLogChannelId);
 						}
-					} else {
-						e.getHook().sendMessage(CONFIG.generic.useEngInsteadOfChin ? "**You do not have permission to use this command!**" : "**你没有权限使用此命令！**").queue();
+
+						Utils.updateBotCommands();
+
+						e.getHook().sendMessage(CONFIG.generic.useEngInsteadOfChin ? "**Config file reloaded successfully!**" : "**配置文件重新加载成功！**").queue();
+					} catch (Exception ex) {
+						LOGGER.error(ExceptionUtils.getStackTrace(ex));
+						e.getHook().sendMessage(CONFIG.generic.useEngInsteadOfChin ? "**Config file reload failed!**" : "**配置文件重新加载失败！**").queue();
 					}
+				} else {
+					e.getHook().sendMessage(CONFIG.generic.useEngInsteadOfChin ? "**You do not have permission to use this command!**" : "**你没有权限使用此命令！**").queue();
 				}
-				case "stop" -> {
-					if (CONFIG.generic.adminsIds.contains(Objects.requireNonNull(e.getMember()).getId())) {
+			}
+			case "console" -> {
+				if (CONFIG.generic.adminsIds.contains(Objects.requireNonNull(e.getMember()).getId())) {
+					String command = Objects.requireNonNull(e.getOption("command")).getAsString();
+					if (command.equals("stop") || command.equals("/stop")) {
 						e.getHook().sendMessage(CONFIG.generic.useEngInsteadOfChin ? "**Stopping the server!**" : "**正在停止服务器！**")
 								.submit()
 								.whenComplete((v, ex) -> SERVER.stop(true));
 					} else {
-						e.getHook().sendMessage(CONFIG.generic.useEngInsteadOfChin ? "**You do not have permission to use this command!**" : "**你没有权限使用此命令！**").queue();
+						e.getHook().sendMessage(CONFIG.generic.useEngInsteadOfChin ? "**Executing the command!**" : "**正在执行命令！**")
+								.submit()
+								.whenComplete((v, ex) -> SERVER.getCommandManager()
+										.execute(SERVER.getCommandSource().withOutput(new DiscordCommandOutput()), command));
 					}
+				} else {
+					e.getHook().sendMessage(CONFIG.generic.useEngInsteadOfChin ? "**You do not have permission to use this command!**" : "**你没有权限使用此命令！**").queue();
 				}
 			}
-		} catch (Exception ex) {
-			LOGGER.error(ExceptionUtils.getStackTrace(ex));
-			e.getHook().sendMessage("**Error!**").queue();
+			case "stop" -> {
+				if (CONFIG.generic.adminsIds.contains(Objects.requireNonNull(e.getMember()).getId())) {
+					e.getHook().sendMessage(CONFIG.generic.useEngInsteadOfChin ? "**Stopping the server!**" : "**正在停止服务器！**")
+							.submit()
+							.whenComplete((v, ex) -> SERVER.stop(true));
+				} else {
+					e.getHook().sendMessage(CONFIG.generic.useEngInsteadOfChin ? "**You do not have permission to use this command!**" : "**你没有权限使用此命令！**").queue();
+				}
+			}
 		}
 	}
 
