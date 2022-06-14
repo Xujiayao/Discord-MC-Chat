70c1b86f917a380531e588485a65edb0ed3d07e2
Xujiayao <xujiayao147258369@gmail.com>
2022-06-14 18:50:32 +0800

    修复自定义进度不翻译的问题
    Fix #35

---
+++ src/main/java/top/xujiayao/mcdiscordchat/minecraft/mixins/MixinLanguage.java
@@ -0,0 +1,76 @@
+package top.xujiayao.mcdiscordchat.minecraft.mixins;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableMap.Builder;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.launch.common.FabricLauncherBase;
+import net.fabricmc.loader.metadata.EntrypointMetadata;
+import net.fabricmc.loader.metadata.LoaderModMetadata;
+import net.minecraft.util.JsonHelper;
+import net.minecraft.util.Language;
+import org.slf4j.Logger;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.regex.Pattern;
+
+/**
+ * @author Xujiayao
+ */
+@Mixin(Language.class)
+public abstract class MixinLanguage {
+
+	@Final
+	@Shadow
+	private static Logger LOGGER;
+
+	@Final
+	@Shadow
+	private static Gson GSON;
+
+	@Final
+	@Shadow
+	private static Pattern TOKEN_PATTERN;
+
+	@SuppressWarnings({"unchecked", "deprecation"})
+	@Redirect(method = "create", at = @At(value = "INVOKE", target = "Lcom/google/common/collect/ImmutableMap$Builder;build()Lcom/google/common/collect/ImmutableMap;"), remap = false)
+	private static <K, V> ImmutableMap<K, V> build(Builder<K, V> builder) {
+		LinkedHashMap<String, String> map = new LinkedHashMap<>((ImmutableMap<String, String>) builder.build());
+
+		FabricLoader.getInstance().getAllMods().forEach(modContainer -> {
+			Optional<? extends EntrypointMetadata> optional = ((LoaderModMetadata) modContainer.getMetadata()).getEntrypoints("main").stream().findFirst();
+			if (optional.isPresent()) {
+				try {
+					try (InputStream inputStream = FabricLauncherBase.getClass(optional.get().getValue()).getResourceAsStream("/assets/" + modContainer.getMetadata().getId() + "/lang/en_us.json")) {
+						if (inputStream == null) {
+							return;
+						}
+
+						JsonObject json = GSON.fromJson(new InputStreamReader(inputStream, StandardCharsets.UTF_8), JsonObject.class);
+						for (Map.Entry<String, JsonElement> entry : json.entrySet()) {
+							String string = TOKEN_PATTERN.matcher(JsonHelper.asString(entry.getValue(), entry.getKey())).replaceAll("%$1s");
+							map.put(entry.getKey(), string);
+						}
+					}
+				} catch (ClassNotFoundException ignored) {
+				} catch (Exception e) {
+					LOGGER.error("Couldn't read strings from /assets/{}", modContainer.getMetadata().getId() + "/lang/en_us.json", e);
+				}
+			}
+		});
+
+		return (ImmutableMap<K, V>) ImmutableMap.builder().putAll(map).build();
+	}
+}
