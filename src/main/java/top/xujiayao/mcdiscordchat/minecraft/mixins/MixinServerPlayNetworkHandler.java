3dc0fd30e7f959a911e31700207bc9dd89ea2cf6
Xujiayao <xujiayao147258369@gmail.com>
2022-06-19 23:33:17 +0800

    修复聊天消息没有显示玩家昵称的问题

--- src/main/java/top/xujiayao/mcdiscordchat/minecraft/mixins/MixinServerPlayNetworkHandler.java
+++ src/main/java/top/xujiayao/mcdiscordchat/minecraft/mixins/MixinServerPlayNetworkHandler.java
@@ -180,9 +180,9 @@ public abstract class MixinServerPlayNetworkHandler {
 					}
 
 					//#if MC >= 11800
-					server.getPlayerManager().broadcast(Text.of(contentToMinecraft), MessageType.CHAT, player.getUuid());
+					server.getPlayerManager().broadcast(new TranslatableText("chat.type.text", player.getDisplayName(), contentToMinecraft), MessageType.CHAT, player.getUuid());
 					//#else
-					//$$ server.getPlayerManager().broadcastChatMessage(Text.of(contentToMinecraft), MessageType.CHAT, player.getUuid());
+					//$$ server.getPlayerManager().broadcastChatMessage(new TranslatableText("chat.type.text", player.getDisplayName(), contentToMinecraft), MessageType.CHAT, player.getUuid());
 					//#endif
 					ci.cancel();
 				}
