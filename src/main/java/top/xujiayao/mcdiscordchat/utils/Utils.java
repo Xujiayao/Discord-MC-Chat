86bbe0a5f707393fd3a98b717d71aba3313f6833
Rustam Gubaydullin <secondfry@yandex-team.ru>
2022-07-01 16:24:36 +0400

    Xujiayao/MCDiscordChat#52: disable auto-embeds for update links

--- src/main/java/top/xujiayao/mcdiscordchat/utils/Utils.java
+++ src/main/java/top/xujiayao/mcdiscordchat/utils/Utils.java
@@ -73,7 +73,7 @@ public class Utils {
 					message.append("\n\n");
 					message.append("MCDiscordChat **").append(VERSION).append("** -> **").append(latestVersion).append("**");
 					message.append("\n\n");
-					message.append(CONFIG.generic.useEngInsteadOfChin ? "Download link: https://github.com/Xujiayao/MCDiscordChat/blob/master/README.md#Download" : "下载链接：https://github.com/Xujiayao/MCDiscordChat/blob/master/README_CN.md#%E4%B8%8B%E8%BD%BD");
+					message.append(CONFIG.generic.useEngInsteadOfChin ? "Download link: <https://github.com/Xujiayao/MCDiscordChat/blob/master/README.md#Download>" : "下载链接：<https://github.com/Xujiayao/MCDiscordChat/blob/master/README_CN.md#%E4%B8%8B%E8%BD%BD>");
 					message.append("\n\n");
 
 					if (CONFIG.generic.updateMessage.equals("full")) {
