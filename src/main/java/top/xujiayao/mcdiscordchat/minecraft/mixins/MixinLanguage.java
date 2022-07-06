b4a322419c3f99fc27eab7ad5d84b37d1d1b6b8f
Xujiayao <xujiayao147258369@gmail.com>
2022-07-06 13:01:22 +0800

    优化代码

--- src/main/java/top/xujiayao/mcdiscordchat/minecraft/mixins/MixinLanguage.java
+++ src/main/java/top/xujiayao/mcdiscordchat/minecraft/mixins/MixinLanguage.java
@@ -7,9 +7,6 @@ import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import net.fabricmc.loader.api.FabricLoader;
-import net.fabricmc.loader.impl.launch.FabricLauncherBase;
-import net.fabricmc.loader.impl.metadata.EntrypointMetadata;
-import net.fabricmc.loader.impl.metadata.LoaderModMetadata;
 import net.minecraft.util.JsonHelper;
 import net.minecraft.util.Language;
 //#if MC >= 11800
@@ -26,6 +23,8 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -55,21 +54,15 @@ public abstract class MixinLanguage {
 		LinkedHashMap<String, String> map = new LinkedHashMap<>((ImmutableMap<String, String>) builder.build());
 
 		FabricLoader.getInstance().getAllMods().forEach(modContainer -> {
-			Optional<? extends EntrypointMetadata> optional = ((LoaderModMetadata) modContainer.getMetadata()).getEntrypoints("main").stream().findFirst();
-			if (optional.isPresent()) {
-				try {
-					try (InputStream inputStream = FabricLauncherBase.getClass(optional.get().getValue()).getResourceAsStream("/assets/" + modContainer.getMetadata().getId() + "/lang/en_us.json")) {
-						if (inputStream == null) {
-							return;
-						}
+			Optional<Path> optional = modContainer.findPath("/assets/" + modContainer.getMetadata().getId() + "/lang/en_us.json");
 
-						JsonObject json = GSON.fromJson(new InputStreamReader(inputStream, StandardCharsets.UTF_8), JsonObject.class);
-						for (Map.Entry<String, JsonElement> entry : json.entrySet()) {
-							String string = TOKEN_PATTERN.matcher(JsonHelper.asString(entry.getValue(), entry.getKey())).replaceAll("%$1s");
-							map.put(entry.getKey(), string);
-						}
+			if (optional.isPresent()) {
+				try (InputStream inputStream = Files.newInputStream(optional.get())) {
+					JsonObject json = GSON.fromJson(new InputStreamReader(inputStream, StandardCharsets.UTF_8), JsonObject.class);
+					for (Map.Entry<String, JsonElement> entry : json.entrySet()) {
+						String string = TOKEN_PATTERN.matcher(JsonHelper.asString(entry.getValue(), entry.getKey())).replaceAll("%$1s");
+						map.put(entry.getKey(), string);
 					}
-				} catch (ClassNotFoundException ignored) {
 				} catch (Exception e) {
 					LOGGER.error("Couldn't read strings from /assets/{}", modContainer.getMetadata().getId() + "/lang/en_us.json", e);
 				}
