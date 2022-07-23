0e09cec3372a973666d79422226f1dd4a70c464b
LofiTurtle <46658877+LofiTurtle@users.noreply.github.com>
2022-07-22 23:00:17 -0700

    Fix ConsoleLogListener.java indentations

--- src/main/java/top/xujiayao/mcdiscordchat/utils/ConsoleLogListener.java
+++ src/main/java/top/xujiayao/mcdiscordchat/utils/ConsoleLogListener.java
@@ -15,81 +15,81 @@ import static top.xujiayao.mcdiscordchat.Main.*;
 
 public class ConsoleLogListener implements Runnable {
 
-    @Override
-    public void run() {
+	@Override
+	public void run() {
 
-        sendLogChannelMessage("**Starting new console log**");
+		sendLogChannelMessage("**Starting new console log**");
 
-        final File file = new File(FabricLoader.getInstance().getGameDir().toString() + "/logs/latest.log");
+		final File file = new File(FabricLoader.getInstance().getGameDir().toString() + "/logs/latest.log");
 
-        try (InputStream is = Files.newInputStream(file.toPath(), StandardOpenOption.READ)) {
-            BufferedReader br = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
-            while (true) {
-                List<String> lines = br.lines().toList();
-                if (!lines.isEmpty()) {
-                    // new messages in log file
-                    ArrayList<String> newMessages = new ArrayList<>();
-                    for (String line : lines) {
-                        // br.lines() doesn't always split on "\n"
-                        newMessages.addAll(new ArrayList<>(Arrays.asList(line.split("\n"))));
-                    }
-                    // logs can get long. split into multiple messages if necessary
-                    StringBuilder messageBatch = new StringBuilder();
-                    Iterator<String> newMessageIterator = newMessages.iterator();
-                    String currentLine = newMessageIterator.next();
-                    boolean finishedSendingMessages = false;
-                    while (!finishedSendingMessages) {
+		try (InputStream is = Files.newInputStream(file.toPath(), StandardOpenOption.READ)) {
+			BufferedReader br = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+			while (true) {
+				List<String> lines = br.lines().toList();
+				if (!lines.isEmpty()) {
+					// new messages in log file
+					ArrayList<String> newMessages = new ArrayList<>();
+					for (String line : lines) {
+						// br.lines() doesn't always split on "\n"
+						newMessages.addAll(new ArrayList<>(Arrays.asList(line.split("\n"))));
+					}
+					// logs can get long. split into multiple messages if necessary
+					StringBuilder messageBatch = new StringBuilder();
+					Iterator<String> newMessageIterator = newMessages.iterator();
+					String currentLine = newMessageIterator.next();
+					boolean finishedSendingMessages = false;
+					while (!finishedSendingMessages) {
 
-                        while (messageBatch.length() + currentLine.length() < 1900) {
-                            // create the message batch
-                            messageBatch.append(currentLine);
-                            messageBatch.append("\n");
-                            if (newMessageIterator.hasNext()) {
-                                currentLine = newMessageIterator.next();
-                            } else {
-                                finishedSendingMessages = true;
-                                break;
-                            }
-                        }
+						while (messageBatch.length() + currentLine.length() < 1900) {
+							// create the message batch
+							messageBatch.append(currentLine);
+							messageBatch.append("\n");
+							if (newMessageIterator.hasNext()) {
+								currentLine = newMessageIterator.next();
+							} else {
+								finishedSendingMessages = true;
+								break;
+							}
+						}
 
-                        if (messageBatch.isEmpty()) {
-                            // currentLine is somehow larger than char limit
-                            messageBatch.append(currentLine);
-                        }
+						if (messageBatch.isEmpty()) {
+							// currentLine is somehow larger than char limit
+							messageBatch.append(currentLine);
+						}
 
-                        messageBatch.deleteCharAt(messageBatch.lastIndexOf("\n"));
-                        sendLogChannelMessage(messageBatch.toString());
-                        messageBatch.delete(0, messageBatch.length());
-                    }
-                }
+						messageBatch.deleteCharAt(messageBatch.lastIndexOf("\n"));
+						sendLogChannelMessage(messageBatch.toString());
+						messageBatch.delete(0, messageBatch.length());
+					}
+				}
 
-                Thread.sleep(1000);
-            }
-        } catch (Exception e) {
-            LOGGER.warn(e.getMessage());
-            LOGGER.info("Closing ConsoleLogListener");
-        }
+				Thread.sleep(1000);
+			}
+		} catch (Exception e) {
+			LOGGER.warn(e.getMessage());
+			LOGGER.info("Closing ConsoleLogListener");
+		}
 
-    }
+	}
 
-    private void sendLogChannelMessage(String message) {
-        if (message.isEmpty()) {
-            return;
-        } else if (message.length() > 1900) {
-            message = message.substring(0, 1900) + "...";
-        }
+	private void sendLogChannelMessage(String message) {
+		if (message.isEmpty()) {
+			return;
+		} else if (message.length() > 1900) {
+			message = message.substring(0, 1900) + "...";
+		}
 
-        if (!CONFIG.generic.consoleLogChannelId.isEmpty()) {
-            if ((System.currentTimeMillis() - MINECRAFT_LAST_RESET_TIME) > 20_000) {
-                MINECRAFT_SEND_COUNT = 0;
-                MINECRAFT_LAST_RESET_TIME = System.currentTimeMillis();
-            }
-            MINECRAFT_SEND_COUNT++;
-            if (MINECRAFT_SEND_COUNT <= 20) {
-                CONSOLE_LOG_CHANNEL.sendMessage(message).queue();
-            }
-        }
-    }
+		if (!CONFIG.generic.consoleLogChannelId.isEmpty()) {
+			if ((System.currentTimeMillis() - MINECRAFT_LAST_RESET_TIME) > 20_000) {
+				MINECRAFT_SEND_COUNT = 0;
+				MINECRAFT_LAST_RESET_TIME = System.currentTimeMillis();
+			}
+			MINECRAFT_SEND_COUNT++;
+			if (MINECRAFT_SEND_COUNT <= 20) {
+				CONSOLE_LOG_CHANNEL.sendMessage(message).queue();
+			}
+		}
+	}
 }
 
 
