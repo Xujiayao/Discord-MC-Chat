1276d1c56fef75fa958097370d310c2031162b3e
Xujiayao <xujiayao147258369@gmail.com>
2022-07-23 14:26:53 +0800

    Optimize code readability and compilation speed

--- src/main/java/top/xujiayao/mcdiscordchat/utils/ConsoleLogListener.java
+++ src/main/java/top/xujiayao/mcdiscordchat/utils/ConsoleLogListener.java
@@ -2,7 +2,10 @@ package top.xujiayao.mcdiscordchat.utils;
 
 import net.fabricmc.loader.api.FabricLoader;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
@@ -11,7 +14,11 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
-import static top.xujiayao.mcdiscordchat.Main.*;
+import static top.xujiayao.mcdiscordchat.Main.CONFIG;
+import static top.xujiayao.mcdiscordchat.Main.CONSOLE_LOG_CHANNEL;
+import static top.xujiayao.mcdiscordchat.Main.LOGGER;
+import static top.xujiayao.mcdiscordchat.Main.MINECRAFT_LAST_RESET_TIME;
+import static top.xujiayao.mcdiscordchat.Main.MINECRAFT_SEND_COUNT;
 
 public class ConsoleLogListener implements Runnable {
 
