e4c5d87ac332d0eee1bbed4346b8f414d7df62ea
Xujiayao <xujiayao147258369@gmail.com>
2022-08-08 16:11:35 +0800

    不实现console，log，stop命令

--- src/main/java/top/xujiayao/mcdiscordchat/minecraft/MinecraftCommands.java
+++ src/main/java/top/xujiayao/mcdiscordchat/minecraft/MinecraftCommands.java
@@ -22,9 +22,9 @@ public class MinecraftCommands {
 	public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
 		dispatcher.register(literal("mcdc").executes(context -> {
 					//#if MC >= 11900
-					context.getSource().sendFeedback(Text.literal(
+					context.getSource().sendFeedback(Text.literal(MarkdownParser.parseMarkdown(
 					//#else
-					//$$ context.getSource().sendFeedback(new LiteralText(
+					//$$ context.getSource().sendFeedback(new LiteralText(MarkdownParser.parseMarkdown(
 					//#endif
 							CONFIG.generic.useEngInsteadOfChin ? """
 									=============== Help ===============
@@ -33,18 +33,22 @@ public class MinecraftCommands {
 									/update              | Check for update
 									/stats <type> <name> | Query the scoreboard of a statistic
 									/reload              | Reload MCDiscordChat config file (admin only)
-									/console <command>   | Execute a command in the server console (admin only)
-									/log                 | Get the specified server log (admin only)
-									/stop                | Stop the server (admin only)""" : """
+									~~/console <command>~~   | Execute a command in the server console (admin only)
+									~~/log~~                 | Get the specified server log (admin only)
+									~~/stop~~                | Stop the server (admin only)
+									
+									*Strikethrough commands are not implemented because they are redundant or impossible to implement in-game.*""" : """
 									=============== 帮助 ===============
 									/info                | 查询服务器运行状态
 									/help                | 获取可用命令列表
 									/update              | 检查更新
 									/stats <type> <name> | 查询该统计信息的排行榜
 									/reload              | 重新加载 MCDiscordChat 配置文件（仅限管理员）
-									/console <command>   | 在服务器控制台中执行命令（仅限管理员）
-									/log                 | 获取指定的服务器日志（仅限管理员）
-									/stop                | 停止服务器（仅限管理员）"""), false);
+									~~/console <command>~~   | 在服务器控制台中执行命令（仅限管理员）
+									~~/log~~                 | 获取指定的服务器日志（仅限管理员）
+									~~/stop~~                | 停止服务器（仅限管理员）
+									
+									*带删除线的命令并没有实现，因为这些命令在游戏内是多余或无法实现的。*""")), false);
 					return 1;
 				})
 				.then(literal("info").executes(context -> {
@@ -62,9 +66,9 @@ public class MinecraftCommands {
 				}))
 				.then(literal("help").executes(context -> {
 					//#if MC >= 11900
-					context.getSource().sendFeedback(Text.literal(
+					context.getSource().sendFeedback(Text.literal(MarkdownParser.parseMarkdown(
 					//#else
-					//$$ context.getSource().sendFeedback(new LiteralText(
+					//$$ context.getSource().sendFeedback(new LiteralText(MarkdownParser.parseMarkdown(
 					//#endif
 							CONFIG.generic.useEngInsteadOfChin ? """
 							=============== Help ===============
@@ -73,18 +77,22 @@ public class MinecraftCommands {
 							/update              | Check for update
 							/stats <type> <name> | Query the scoreboard of a statistic
 							/reload              | Reload MCDiscordChat config file (admin only)
-							/console <command>   | Execute a command in the server console (admin only)
-							/log                 | Get the specified server log (admin only)
-							/stop                | Stop the server (admin only)""" : """
+							~~/console <command>~~   | Execute a command in the server console (admin only)
+							~~/log~~                 | Get the specified server log (admin only)
+							~~/stop~~                | Stop the server (admin only)
+							
+							*Strikethrough commands are not implemented because they are redundant or impossible to implement in-game.*""" : """
 							=============== 帮助 ===============
 							/info                | 查询服务器运行状态
 							/help                | 获取可用命令列表
 							/update              | 检查更新
 							/stats <type> <name> | 查询该统计信息的排行榜
 							/reload              | 重新加载 MCDiscordChat 配置文件（仅限管理员）
-							/console <command>   | 在服务器控制台中执行命令（仅限管理员）
-							/log                 | 获取指定的服务器日志（仅限管理员）
-							/stop                | 停止服务器（仅限管理员）"""), false);
+							~~/console <command>~~   | 在服务器控制台中执行命令（仅限管理员）
+							~~/log~~                 | 获取指定的服务器日志（仅限管理员）
+							~~/stop~~                | 停止服务器（仅限管理员）
+							
+							*带删除线的命令并没有实现，因为这些命令在游戏内是多余或无法实现的。*""")), false);
 					return 1;
 				}))
 				.then(literal("update").executes(context -> {
