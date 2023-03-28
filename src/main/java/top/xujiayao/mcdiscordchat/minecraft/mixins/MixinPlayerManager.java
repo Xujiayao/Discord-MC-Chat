c58398e022c6ffc28f7926ca990553e639ccf63f
Xujiayao <xujiayao147258369@gmail.com>
2023-03-28 23:49:31 +0800

    发送/say命令的聊天消息至Discord
    Fix #125

--- src/main/java/top/xujiayao/mcdiscordchat/minecraft/mixins/MixinPlayerManager.java
+++ src/main/java/top/xujiayao/mcdiscordchat/minecraft/mixins/MixinPlayerManager.java
@@ -1,9 +1,24 @@
 package top.xujiayao.mcdiscordchat.minecraft.mixins;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import net.dv8tion.jda.api.utils.MarkdownSanitizer;
 import net.minecraft.network.ClientConnection;
+import net.minecraft.network.message.MessageType;
+//#if MC >= 11900
+import net.minecraft.network.message.SignedMessage;
+//#endif
 import net.minecraft.server.PlayerManager;
+import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
+//#if MC < 11900
+//$$ import net.minecraft.text.Text;
+//#endif
+import okhttp3.MediaType;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,8 +26,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import top.xujiayao.mcdiscordchat.utils.Translations;
 import top.xujiayao.mcdiscordchat.utils.Utils;
 
+//#if MC < 11900
+//$$ import java.util.UUID;
+//#endif
+
 import static top.xujiayao.mcdiscordchat.Main.CHANNEL;
 import static top.xujiayao.mcdiscordchat.Main.CONFIG;
+import static top.xujiayao.mcdiscordchat.Main.HTTP_CLIENT;
+import static top.xujiayao.mcdiscordchat.Main.LOGGER;
 import static top.xujiayao.mcdiscordchat.Main.MULTI_SERVER;
 
 /**
@@ -20,6 +41,65 @@ import static top.xujiayao.mcdiscordchat.Main.MULTI_SERVER;
  */
 @Mixin(PlayerManager.class)
 public class MixinPlayerManager {
+
+	//#if MC >= 11903
+	@Inject(method = "broadcast(Lnet/minecraft/network/message/SignedMessage;Lnet/minecraft/server/command/ServerCommandSource;Lnet/minecraft/network/message/MessageType$Parameters;)V", at = @At("RETURN"))
+	private void broadcast(SignedMessage message, ServerCommandSource source, MessageType.Parameters params, CallbackInfo ci) {
+		sendMessage(message.getSignedContent(), source.getName());
+	}
+	//#elseif MC >= 11900
+	//$$ @Inject(method = "broadcast(Lnet/minecraft/network/message/SignedMessage;Lnet/minecraft/server/command/ServerCommandSource;Lnet/minecraft/network/message/MessageType$Parameters;)V", at = @At("RETURN"))
+	//$$ private void broadcast(SignedMessage message, ServerCommandSource source, MessageType.Parameters params, CallbackInfo ci) {
+	//$$  sendMessage(message.getSignedContent().plain(), source.getName());
+	//$$ }
+	//#elseif MC >= 11800
+	//$$ @Inject(method = "broadcast", at = @At("RETURN"))
+	//$$ private void broadcast(Text message, MessageType type, UUID sender, CallbackInfo ci) {
+	//$$  sendMessage(message.getString(), "Server");
+	//$$ }
+	//#elseif MC >= 11600
+	//$$ @Inject(method = "broadcastChatMessage", at = @At("RETURN"))
+	//$$ private void broadcastChatMessage(Text message, MessageType type, UUID sender, CallbackInfo ci) {
+	//$$  sendMessage(message.getString(), "Server");
+	//$$ }
+	//#else
+	//$$ @Inject(method = "sendToAll(Lnet/minecraft/text/Text;)V", at = @At("RETURN"))
+	//$$ private void sendToAll(Text text, CallbackInfo ci) {
+	//$$  sendMessage(text.getString(), "Server");
+	//$$ }
+	//#endif
+
+	private void sendMessage(String content, String username) {
+		if (CONFIG.generic.broadcastChatMessages) {
+			if (CONFIG.generic.webhookUrl.isBlank()) {
+				CHANNEL.sendMessage(((CONFIG.multiServer.enable) ? ("[" + CONFIG.multiServer.name + "] <") : "<") + username + "> " + content).queue();
+			} else {
+				JsonObject body = new JsonObject();
+				body.addProperty("content", content);
+				body.addProperty("username", ((CONFIG.multiServer.enable) ? ("[" + CONFIG.multiServer.name + "] " + username) : username));
+				body.addProperty("avatar_url", "https://cdn.jsdelivr.net/gh/Xujiayao/MCDiscordChat@master/src/main/resources/assets/mcdiscordchat/icon.png");
+				if (!CONFIG.generic.allowMentions) {
+					body.add("allowed_mentions", new Gson().fromJson("{\"parse\":[]}", JsonObject.class));
+				}
+
+				Request request = new Request.Builder()
+						.url(CONFIG.generic.webhookUrl)
+						.post(RequestBody.create(body.toString(), MediaType.get("application/json")))
+						.build();
+
+				try {
+					Response response = HTTP_CLIENT.newCall(request).execute();
+					response.close();
+				} catch (Exception e) {
+					LOGGER.error(ExceptionUtils.getStackTrace(e));
+				}
+			}
+
+			if (CONFIG.multiServer.enable) {
+				MULTI_SERVER.sendMessage(false, true, false, username, content);
+			}
+		}
+	}
 
 	@Inject(method = "onPlayerConnect", at = @At("RETURN"))
 	private void onPlayerConnect(ClientConnection connection, ServerPlayerEntity player, CallbackInfo ci) {
