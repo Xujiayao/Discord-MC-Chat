33ec481d057368583563916f58d052312e80a018
Xujiayao <xujiayao147258369@gmail.com>
2023-09-24 00:52:13 +0800

    在线下载语言文件以简化空间

--- src/main/java/top/xujiayao/mcdiscordchat/utils/Translations.java
+++ src/main/java/top/xujiayao/mcdiscordchat/utils/Translations.java
@@ -3,10 +3,17 @@ package top.xujiayao.mcdiscordchat.utils;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.SharedConstants;
 import net.minecraft.util.Language;
+import okhttp3.CacheControl;
+import okhttp3.Request;
+import okhttp3.Response;
+import okio.BufferedSink;
+import okio.Okio;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 
+import java.io.File;
 import java.lang.reflect.Field;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -17,6 +24,7 @@ import java.util.Map;
 import java.util.Optional;
 
 import static top.xujiayao.mcdiscordchat.Main.CONFIG;
+import static top.xujiayao.mcdiscordchat.Main.HTTP_CLIENT;
 import static top.xujiayao.mcdiscordchat.Main.LOGGER;
 
 /**
@@ -46,9 +54,50 @@ public class Translations {
 
 		if (optional.isPresent()) {
 			try {
-				String content = IOUtils.toString(Files.newInputStream(optional.get()), StandardCharsets.UTF_8);
-				translations = new Gson().fromJson(content, new TypeToken<Map<String, String>>() {
-				}.getType());
+				String mcdcLang = IOUtils.toString(Files.newInputStream(optional.get()), StandardCharsets.UTF_8);
+				translations.putAll(new Gson().fromJson(mcdcLang, new TypeToken<Map<String, String>>() {
+				}.getType()));
+
+				File langFolder = new File(FabricLoader.getInstance().getConfigDir().toFile(), "/mcdiscordchat/");
+				if (!(langFolder.mkdirs() || langFolder.isDirectory())) {
+					return;
+				}
+
+				File minecraftLangFile = new File(FabricLoader.getInstance().getConfigDir().toFile(), "/mcdiscordchat/" + SharedConstants.getGameVersion().getName() + "-" + CONFIG.generic.language + ".json");
+
+				if (minecraftLangFile.length() == 0) {
+					Request request1 = new Request.Builder()
+							.url("https://cdn.jsdelivr.net/gh/InventivetalentDev/minecraft-assets@" + SharedConstants.getGameVersion().getName() + "/assets/minecraft/lang/" + CONFIG.generic.language + ".json")
+							.cacheControl(CacheControl.FORCE_NETWORK)
+							.build();
+
+					try (Response response1 = HTTP_CLIENT.newCall(request1).execute()) {
+						if (response1.body() != null && response1.code() == 200) {
+							BufferedSink sink = Okio.buffer(Okio.sink(minecraftLangFile));
+							sink.writeAll(response1.body().source());
+							sink.close();
+						} else if (response1.code() == 404) {
+							minecraftLangFile = new File(FabricLoader.getInstance().getConfigDir().toFile(), "/mcdiscordchat/latest-" + CONFIG.generic.language + ".json");
+
+							Request request2 = new Request.Builder()
+									.url("https://cdn.jsdelivr.net/gh/InventivetalentDev/minecraft-assets@latest/assets/minecraft/lang/" + CONFIG.generic.language + ".json")
+									.cacheControl(CacheControl.FORCE_NETWORK)
+									.build();
+
+							try (Response response2 = HTTP_CLIENT.newCall(request2).execute()) {
+								if (response2.body() != null && response2.code() == 200) {
+									BufferedSink sink = Okio.buffer(Okio.sink(minecraftLangFile));
+									sink.writeAll(response2.body().source());
+									sink.close();
+								}
+							}
+						}
+					}
+				}
+
+				String minecraftLang = IOUtils.toString(minecraftLangFile.toURI(), StandardCharsets.UTF_8);
+				translations.putAll(new Gson().fromJson(minecraftLang, new TypeToken<Map<String, String>>() {
+				}.getType()));
 			} catch (Exception e) {
 				LOGGER.error(ExceptionUtils.getStackTrace(e));
 			}
