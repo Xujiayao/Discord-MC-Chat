d0484ee1daeb7d2354d35b8a03256c035ad58803
Xujiayao <i@xujiayao.com>
2024-02-08 20:45:57 +0800

    处理 Message

---
+++ src/main/java/com/xujiayao/mcdiscordchat/minecraft/mixins/MixinServerGamePacketListenerImpl.java
@@ -0,0 +1,36 @@
+package com.xujiayao.mcdiscordchat.minecraft.mixins;
+
+import com.xujiayao.mcdiscordchat.minecraft.MinecraftEvents;
+import net.minecraft.network.chat.ChatType;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.PlayerChatMessage;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.network.ServerGamePacketListenerImpl;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Optional;
+
+import static com.xujiayao.mcdiscordchat.Main.SERVER;
+
+/**
+ * @author Xujiayao
+ */
+@Mixin(ServerGamePacketListenerImpl.class)
+public class MixinServerGamePacketListenerImpl {
+
+	@Shadow
+	private ServerPlayer player;
+
+	@Inject(method = "broadcastChatMessage", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/players/PlayerList;broadcastChatMessage(Lnet/minecraft/network/chat/PlayerChatMessage;Lnet/minecraft/server/level/ServerPlayer;Lnet/minecraft/network/chat/ChatType$Bound;)V"), cancellable = true)
+	private void broadcastChatMessage(PlayerChatMessage playerChatMessage, CallbackInfo ci) {
+		Optional<Component> result = MinecraftEvents.PLAYER_MESSAGE.invoker().message(player, playerChatMessage);
+		if (result.isPresent()) {
+			SERVER.getPlayerList().broadcastChatMessage(playerChatMessage.withUnsignedContent(result.get()), this.player, ChatType.bind(ChatType.CHAT, player));
+			ci.cancel();
+		}
+	}
+}
