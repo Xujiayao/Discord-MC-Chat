979f18ffc4331d7bdea68235b0742207d0ef44f8
Xujiayao <i@xujiayao.com>
2024-02-11 03:54:45 +0800

    MixinLanguage

---
+++ src/main/java/com/xujiayao/mcdiscordchat/minecraft/mixins/MixinLanguage.java
@@ -0,0 +1,13 @@
+//#if MC >= 11900
+package com.xujiayao.mcdiscordchat.minecraft.mixins;
+
+import net.minecraft.locale.Language;
+import org.spongepowered.asm.mixin.Mixin;
+
+/**
+ * @author Xujiayao
+ */
+@Mixin(Language.class)
+public class MixinLanguage {
+}
+//#endif
