ca656ef3b6ab2be74858ddc72c983443bf955608
Xujiayao <i@xujiayao.com>
2024-07-10 00:22:53 +0800

    修复roleColor颜色parse失败

--- src/main/java/com/xujiayao/discord_mc_chat/utils/PlaceholderParser.java
+++ src/main/java/com/xujiayao/discord_mc_chat/utils/PlaceholderParser.java
@@ -4,6 +4,7 @@ import eu.pb4.placeholders.api.PlaceholderContext;
 import eu.pb4.placeholders.api.node.TextNode;
 import eu.pb4.placeholders.api.node.parent.ClickActionNode;
 import eu.pb4.placeholders.api.node.parent.FormattingNode;
+import eu.pb4.placeholders.api.node.parent.HoverNode;
 import eu.pb4.placeholders.api.parsers.MarkdownLiteParserV1;
 import eu.pb4.placeholders.api.parsers.NodeParser;
 import eu.pb4.placeholders.api.parsers.ParserBuilder;
@@ -11,9 +12,6 @@ import eu.pb4.placeholders.api.parsers.TagLikeParser;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.ClickEvent;
 import net.minecraft.network.chat.Component;
-//#if MC < 11900
-//$$ import net.minecraft.network.chat.TextComponent;
-//#endif
 
 import java.util.Map;
 
@@ -41,88 +39,62 @@ public class PlaceholderParser {
 	}
 
 	public static Component parseOtherMessage(String server, Component message) {
-		Map<String, Component> placeholders = Map.of(
-				//#if MC >= 11900
-				"server", Component.literal(server),
-				"message", message
-				//#else
-				//$$ "server", new TextComponent(server),
-				//$$ "message", message
-				//#endif
+		Map<String, TextNode> placeholders = Map.of(
+				"server", TextNode.of(server),
+				"message", TextNode.convert(message)
 		);
 
 		NodeParser parser = ParserBuilder.of()
 				.add(PARSER)
-				.customTags(TagLikeParser.PLACEHOLDER_ALTERNATIVE, TagLikeParser.Provider.placeholderText(placeholders::get))
+				.customTags(TagLikeParser.PLACEHOLDER_ALTERNATIVE, TagLikeParser.Provider.placeholder(placeholders::get))
 				.build();
 
 		return parser.parseText(TextNode.of(Translations.translateMessage("message.otherMessage")), PlaceholderContext.of(SERVER).asParserContext());
 	}
 
 	public static Component parseCommandNotice(String name, String roleName, String roleColor, String command) {
-		Map<String, Component> placeholders = Map.of(
-				//#if MC >= 11900
-				"name", Component.literal(name),
-				"roleName", Component.literal(roleName),
-				"roleColor", Component.literal(roleColor),
-				"command", Component.literal(command)
-				//#else
-				//$$ "name", new TextComponent(name),
-				//$$ "roleName", new TextComponent(roleName),
-				//$$ "roleColor", new TextComponent(roleColor),
-				//$$ "command", new TextComponent(command)
-				//#endif
+		Map<String, TextNode> placeholders = Map.of(
+				"name", TextNode.of(name),
+				"roleName", TextNode.of(roleName),
+				"roleColor", TextNode.of(roleColor),
+				"command", TextNode.of(command)
 		);
 
 		NodeParser parser = ParserBuilder.of()
 				.add(PARSER)
-				.customTags(TagLikeParser.PLACEHOLDER_ALTERNATIVE, TagLikeParser.Provider.placeholderText(placeholders::get))
+				.customTags(TagLikeParser.PLACEHOLDER_ALTERNATIVE, TagLikeParser.Provider.placeholder(placeholders::get))
 				.build();
 
 		return parser.parseText(TextNode.of(Translations.translateMessage("message.commandNotice")), PlaceholderContext.of(SERVER).asParserContext());
 	}
 
 	public static Component parseResponseMessage(String server, String name, String roleName, String message) {
-		Map<String, Component> placeholders = Map.of(
-				//#if MC >= 11900
-				"server", Component.literal(server),
-				"name", Component.literal(name),
-				"roleName", Component.literal(roleName),
-				"message", Component.literal(message)
-				//#else
-				//$$ "server", new TextComponent(server),
-				//$$ "name", new TextComponent(name),
-				//$$ "roleName", new TextComponent(roleName),
-				//$$ "message", new TextComponent(message)
-				//#endif
+		Map<String, TextNode> placeholders = Map.of(
+				"server", TextNode.of(server),
+				"name", TextNode.of(name),
+				"roleName", TextNode.of(roleName),
+				"message", TextNode.of(message)
 		);
 
 		NodeParser parser = ParserBuilder.of()
 				.add(PARSER)
-				.customTags(TagLikeParser.PLACEHOLDER_ALTERNATIVE, TagLikeParser.Provider.placeholderText(placeholders::get))
+				.customTags(TagLikeParser.PLACEHOLDER_ALTERNATIVE, TagLikeParser.Provider.placeholder(placeholders::get))
 				.build();
 
 		return parser.parseText(TextNode.of(Translations.translateMessage("message.responseMessage")), PlaceholderContext.of(SERVER).asParserContext());
 	}
 
 	public static Component parseChatMessage(String server, String name, String roleName, String message) {
-		Map<String, Component> placeholders = Map.of(
-				//#if MC >= 11900
-				"server", Component.literal(server),
-				"name", Component.literal(name),
-				"roleName", Component.literal(roleName),
-				"message", Component.literal(message)
-				//#else
-				//$$ "server", new TextComponent(server),
-				//$$ "name", new TextComponent(name),
-				//$$ "roleName", new TextComponent(roleName),
-				//$$ "message", new TextComponent(message)
-				//#endif
+		Map<String, TextNode> placeholders = Map.of(
+				"server", TextNode.of(server),
+				"name", TextNode.of(name),
+				"roleName", TextNode.of(roleName),
+				"message", TextNode.of(message)
 		);
 
 		NodeParser parser = ParserBuilder.of()
 				.add(PARSER)
-				.customTags(TagLikeParser.PLACEHOLDER_ALTERNATIVE, TagLikeParser.Provider.placeholderText(placeholders::get))
+				.customTags(TagLikeParser.PLACEHOLDER_ALTERNATIVE, TagLikeParser.Provider.placeholder(placeholders::get))
 				.build();
 
 		return parser.parseText(TextNode.of(Translations.translateMessage("message.chatMessage")), PlaceholderContext.of(SERVER).asParserContext());
