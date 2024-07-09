ebb0e7925ddb6c2d4ee0678033ec13bf1bfa34f4
Xujiayao <i@xujiayao.com>
2024-07-09 16:18:16 +0800

    配置文件不再需要区分formatted和unformatted

--- src/main/java/com/xujiayao/discord_mc_chat/Config.java
+++ src/main/java/com/xujiayao/discord_mc_chat/Config.java
@@ -81,15 +81,20 @@ public class Config {
 
 	@SuppressWarnings("unused")
 	public static class CustomMessage {
-		public String unformattedResponseMessage = "";
-		public String unformattedChatMessage = "";
-		public String unformattedOtherMessage = "";
-		public String unformattedCommandNotice = "";
+		public String responseMessage = "";
+		public String chatMessage = "";
+		public String otherMessage = "";
+		public String commandNotice = "";
 
-		public String formattedResponseMessage = "";
-		public String formattedChatMessage = "";
-		public String formattedOtherMessage = "";
-		public String formattedCommandNotice = "";
+		/* Not Used */ public String unformattedResponseMessage = "";
+		/* Not Used */ public String unformattedChatMessage = "";
+		/* Not Used */ public String unformattedOtherMessage = "";
+		/* Not Used */ public String unformattedCommandNotice = "";
+
+		/* Not Used */ public String formattedResponseMessage = "";
+		/* Not Used */ public String formattedChatMessage = "";
+		/* Not Used */ public String formattedOtherMessage = "";
+		/* Not Used */ public String formattedCommandNotice = "";
 
 		public String messageWithoutWebhook = "";
 		public String messageWithoutWebhookForMultiServer = "";
