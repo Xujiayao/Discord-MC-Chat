42a996ea247cdc7a64d6c0b21bfdb33313d6ea0b
Xujiayao <i@xujiayao.com>
2025-07-31 21:16:54 +0800

    Fabric NeoForge setup

---
+++ fabric/src/main/java/com/xujiayao/discord_mc_chat/fabric/mixin/ExampleMixin.java
@@ -0,0 +1,18 @@
+package com.xujiayao.discord_mc_chat.fabric.mixin;
+
+import net.minecraft.server.MinecraftServer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import static com.xujiayao.discord_mc_chat.fabric.DiscordMCChat.LOGGER;
+
+@Mixin(MinecraftServer.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "loadLevel")
+	private void init(CallbackInfo info) {
+		// This code is injected into the start of MinecraftServer.loadLevel()V
+		LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
