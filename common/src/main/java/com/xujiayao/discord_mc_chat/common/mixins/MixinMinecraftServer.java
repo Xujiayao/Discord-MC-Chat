a9e56406731eda8c3ec5f74c3d0f5615014ea841
Xujiayao <i@xujiayao.com>
2025-08-04 11:10:33 +0800

    new mixin

---
+++ common/src/main/java/com/xujiayao/discord_mc_chat/common/mixins/MixinMinecraftServer.java
@@ -0,0 +1,16 @@
+package com.xujiayao.discord_mc_chat.common.mixins;
+
+import net.minecraft.server.MinecraftServer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MinecraftServer.class)
+public class MixinMinecraftServer {
+	@Inject(at = @At("HEAD"), method = "loadLevel")
+	private void init(CallbackInfo info) {
+		// This code is injected into the start of MinecraftServer.loadLevel()V
+		System.out.println("This line is printed by an example mod mixin!");
+	}
+}
