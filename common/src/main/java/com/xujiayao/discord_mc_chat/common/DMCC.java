85758a4198207805a1c5df95a176c1abdef83dd0
Xujiayao <i@xujiayao.com>
2025-08-04 15:38:51 +0800

    add dependency

--- common/src/main/java/com/xujiayao/discord_mc_chat/common/DMCC.java
+++ common/src/main/java/com/xujiayao/discord_mc_chat/common/DMCC.java
@@ -1,5 +1,8 @@
 package com.xujiayao.discord_mc_chat.common;
 
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
 public class DMCC {
 
 	public static void main(String[] args) {
@@ -8,5 +11,20 @@ public class DMCC {
 
 	public static void init(String loader) {
 		System.out.println("Initializing DMCC with loader: " + loader);
+
+		try {
+			// TOKEN should be replaced with your Discord bot token
+			String token = "";
+			JDA jda = JDABuilder.createDefault(token)
+					.addEventListeners(new Discord())
+					.build();
+			jda.awaitReady();
+			System.out.println("PingPongBot is ready!");
+		} catch (Exception e) {
+			System.err.println("Failed to initialize DMCC: " + e.getMessage());
+			e.printStackTrace();
+		} finally {
+			System.out.println("DMCC initialization complete.");
+		}
 	}
 }
---
+++ common/src/main/java/com/xujiayao/discord_mc_chat/common/Discord.java
@@ -0,0 +1,23 @@
+package com.xujiayao.discord_mc_chat.common;
+
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class Discord extends ListenerAdapter {
+
+	@Override
+	public void onMessageReceived(MessageReceivedEvent event) {
+		Message message = event.getMessage();
+		String content = message.getContentRaw();
+		User author = event.getAuthor();
+
+		// Ignore bot messages
+		if (author.isBot()) return;
+
+		if (content.equalsIgnoreCase("!ping")) {
+			event.getChannel().sendMessage("Pong!").queue();
+		}
+	}
+}
