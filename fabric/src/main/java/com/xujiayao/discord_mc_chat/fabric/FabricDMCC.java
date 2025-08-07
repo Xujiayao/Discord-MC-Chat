a96defcf32408576da2c41b8f8ae3f2d5e612d0c
Xujiayao <i@xujiayao.com>
2025-08-07 14:51:40 +0800

    no logger needed for mod init

--- fabric/src/main/java/com/xujiayao/discord_mc_chat/fabric/FabricDMCC.java
+++ fabric/src/main/java/com/xujiayao/discord_mc_chat/fabric/FabricDMCC.java
@@ -2,25 +2,14 @@ package com.xujiayao.discord_mc_chat.fabric;
 
 import com.xujiayao.discord_mc_chat.common.DMCC;
 import net.fabricmc.api.ModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class FabricDMCC implements ModInitializer {
-
-	public static final String MOD_ID = "discord_mc_chat";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
 
 		DMCC.init("Fabric");
 	}
--- neoforge/src/main/java/com/xujiayao/discord_mc_chat/neoforge/NeoForgeDMCC.java
+++ neoforge/src/main/java/com/xujiayao/discord_mc_chat/neoforge/NeoForgeDMCC.java
@@ -2,23 +2,14 @@ package com.xujiayao.discord_mc_chat.neoforge;
 
 import com.xujiayao.discord_mc_chat.common.DMCC;
 import net.neoforged.fml.common.Mod;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
-@Mod(NeoForgeDMCC.MOD_ID)
+@Mod("discord_mc_chat")
 public class NeoForgeDMCC {
-
-	// Define mod id in a common place for everything to reference
-	public static final String MOD_ID = "discord_mc_chat";
-	// Directly reference a slf4j logger
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	// The constructor for the mod class is the first code that is run when your mod is loaded.
 	// FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
 	public NeoForgeDMCC() {
-		LOGGER.info("Hello NeoForge world!");
-
 		DMCC.init("NeoForge");
 	}
 }
