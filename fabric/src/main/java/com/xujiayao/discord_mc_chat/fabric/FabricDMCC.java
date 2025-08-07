f056c896fa1f89858fb94ff5e203c58c5b963716
Xujiayao <i@xujiayao.com>
2025-08-07 15:07:20 +0800

    rm comments

--- fabric/src/main/java/com/xujiayao/discord_mc_chat/fabric/FabricDMCC.java
+++ fabric/src/main/java/com/xujiayao/discord_mc_chat/fabric/FabricDMCC.java
@@ -7,10 +7,6 @@ public class FabricDMCC implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
 		DMCC.init("Fabric");
 	}
 }
--- neoforge/src/main/java/com/xujiayao/discord_mc_chat/neoforge/NeoForgeDMCC.java
+++ neoforge/src/main/java/com/xujiayao/discord_mc_chat/neoforge/NeoForgeDMCC.java
@@ -3,12 +3,9 @@ package com.xujiayao.discord_mc_chat.neoforge;
 import com.xujiayao.discord_mc_chat.common.DMCC;
 import net.neoforged.fml.common.Mod;
 
-// The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod("discord_mc_chat")
 public class NeoForgeDMCC {
 
-	// The constructor for the mod class is the first code that is run when your mod is loaded.
-	// FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
 	public NeoForgeDMCC() {
 		DMCC.init("NeoForge");
 	}
