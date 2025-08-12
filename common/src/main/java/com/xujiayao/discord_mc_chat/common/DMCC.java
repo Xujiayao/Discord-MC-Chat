8b9f951218cd9a9f8904ca001e82945ccd448e7d
Xujiayao <i@xujiayao.com>
2025-08-12 16:20:35 +0800

    rm discord

--- common/src/main/java/com/xujiayao/discord_mc_chat/common/DMCC.java
+++ common/src/main/java/com/xujiayao/discord_mc_chat/common/DMCC.java
@@ -2,9 +2,6 @@ package com.xujiayao.discord_mc_chat.common;
 
 import com.xujiayao.discord_mc_chat.common.utils.Logger;
 import com.xujiayao.discord_mc_chat.common.utils.Utils;
-import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.requests.GatewayIntent;
 
 /**
  * @author Xujiayao
@@ -22,19 +19,5 @@ public class DMCC {
 
 	public static void init(String loader) {
 		LOGGER.info("Initializing DMCC {} with loader: {}", VERSION, loader);
-
-		try {
-			String token = ""; // intended to be empty
-			JDA jda = JDABuilder.createDefault(token)
-					.enableIntents(GatewayIntent.MESSAGE_CONTENT)
-					.addEventListeners(new Discord())
-					.build();
-			jda.awaitReady();
-			LOGGER.info("PingPongBot is ready!");
-		} catch (Exception e) {
-			LOGGER.error("Failed to initialize DMCC: {}", e.getMessage());
-		} finally {
-			LOGGER.info("DMCC initialization complete.");
-		}
 	}
 }
