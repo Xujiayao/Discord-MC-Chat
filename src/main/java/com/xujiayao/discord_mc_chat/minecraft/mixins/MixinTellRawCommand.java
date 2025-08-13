223d741c2aeb573b611ac23616dc18f9a063b6c3
Xujiayao <i@xujiayao.com>
2025-08-13 10:20:10 +0800

    tellraw支持处理selector

--- src/main/java/com/xujiayao/discord_mc_chat/minecraft/mixins/MixinTellRawCommand.java
+++ src/main/java/com/xujiayao/discord_mc_chat/minecraft/mixins/MixinTellRawCommand.java
@@ -4,11 +4,17 @@ import com.mojang.brigadier.context.CommandContext;
 import com.xujiayao.discord_mc_chat.minecraft.MinecraftEvents;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.arguments.ComponentArgument;
+//#if MC < 12105
+//$$ import net.minecraft.network.chat.ComponentUtils;
+//#endif
 import net.minecraft.server.commands.TellRawCommand;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import static com.xujiayao.discord_mc_chat.Main.LOGGER;
 
 /**
  * @author Xujiayao
@@ -21,11 +27,15 @@ public class MixinTellRawCommand {
 		String input = context.getInput();
 
 		if (input.startsWith("/tellraw @a ") || input.startsWith("tellraw @a ")) {
-			//#if MC >= 12105
-			MinecraftEvents.COMMAND_MESSAGE.invoker().message(ComponentArgument.getRawComponent(context, "message").getString(), context.getSource());
-			//#else
-			//$$ MinecraftEvents.COMMAND_MESSAGE.invoker().message(ComponentArgument.getComponent(context, "message").getString(), context.getSource());
-			//#endif
+			try {
+				//#if MC >= 12105
+				MinecraftEvents.COMMAND_MESSAGE.invoker().message(ComponentArgument.getResolvedComponent(context, "message").getString(), context.getSource());
+				//#else
+				//$$ MinecraftEvents.COMMAND_MESSAGE.invoker().message(ComponentUtils.updateForEntity(context.getSource(), ComponentArgument.getComponent(context, "message"), context.getSource().getEntity(), 0).getString(), context.getSource());
+				//#endif
+			} catch (Exception e) {
+				LOGGER.error(ExceptionUtils.getStackTrace(e));
+			}
 		}
 	}
 }
