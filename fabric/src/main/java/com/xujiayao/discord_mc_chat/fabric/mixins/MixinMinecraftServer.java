19620c4fd9ea204f7c71032863424307bcd342fa
Xujiayao <i@xujiayao.com>
2025-09-09 00:17:35 +0800

    server start event

---
+++ fabric/src/main/java/com/xujiayao/discord_mc_chat/fabric/mixins/MixinMinecraftServer.java
@@ -0,0 +1,22 @@
+package com.xujiayao.discord_mc_chat.fabric.mixins;
+
+import net.minecraft.server.MinecraftServer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import static com.xujiayao.discord_mc_chat.common.DMCC.LOGGER;
+
+/**
+ * @author Xujiayao
+ */
+@Mixin(MinecraftServer.class)
+public class MixinMinecraftServer {
+
+	@Inject(method = "runServer", at = @At(value = "INVOKE", target = "Lnet/minecraft/Util;getNanos()J", ordinal = 0))
+	private void afterSetupServer(CallbackInfo ci) {
+		// ServerStart Event
+		LOGGER.info("[DMCC] Server started");
+	}
+}
