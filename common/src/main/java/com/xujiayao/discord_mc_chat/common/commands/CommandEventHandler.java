bfa5a3f27a5f367b856966a907a348468f210707
Xujiayao <i@xujiayao.com>
2025-09-21 23:07:41 +0800

    translate

--- common/src/main/java/com/xujiayao/discord_mc_chat/common/commands/CommandEventHandler.java
+++ common/src/main/java/com/xujiayao/discord_mc_chat/common/commands/CommandEventHandler.java
@@ -2,6 +2,7 @@ package com.xujiayao.discord_mc_chat.common.commands;
 
 import com.xujiayao.discord_mc_chat.common.DMCC;
 import com.xujiayao.discord_mc_chat.common.utils.events.EventManager;
+import com.xujiayao.discord_mc_chat.common.utils.i18n.I18nManager;
 
 import static com.xujiayao.discord_mc_chat.common.DMCC.IS_MINECRAFT_ENV;
 import static com.xujiayao.discord_mc_chat.common.DMCC.LOGGER;
@@ -18,12 +19,12 @@ public class CommandEventHandler {
 	 */
 	public static void init() {
 		EventManager.register(CommandEvents.ReloadEvent.class, event -> new Thread(() -> {
-			LOGGER.info("Reloading DMCC...");
+			LOGGER.info(I18nManager.getDmccTranslation("commands.reload.reloading"));
 			DMCC.reload();
 		}, "DMCC-Command").start());
 
 		EventManager.register(CommandEvents.StopEvent.class, event -> new Thread(() -> {
-			LOGGER.info("Stopping DMCC...");
+			LOGGER.info(I18nManager.getDmccTranslation("commands.stop.stopping"));
 
 			if (IS_MINECRAFT_ENV) {
 				LOGGER.info("Run \"/dmcc start\" to start DMCC again");
