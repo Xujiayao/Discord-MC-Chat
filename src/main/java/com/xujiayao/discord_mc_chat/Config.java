6bd3bcac598a4adc7bd3c8d5e4cd3f861f53523d
Jason Xu <i@xujiayao.com>
2025-10-30 15:40:31 +0800

    also these

--- src/main/java/com/xujiayao/discord_mc_chat/Config.java
+++ src/main/java/com/xujiayao/discord_mc_chat/Config.java
@@ -24,7 +24,7 @@ public class Config {
 		public String botToken = "";
 
 		public boolean showServerStatusInBotStatus = true;
-		public String botPlayingActivity = "Minecraft (%onlinePlayerCount% / %maxPlayerCount%)";
+		public String botPlayingActivity = "Minecraft (%onlinePlayerCount%/%maxPlayerCount%)";
 		public String botListeningActivity = "";
 
 		public boolean useWebhook = true;
