2b83f2b9d2c63e5407ad754f2722d609fab22165
Xujiayao <i@xujiayao.com>
2025-11-01 00:05:47 +0800

    允许禁用转发通过/tellraw @a发送的聊天消息

--- src/main/java/com/xujiayao/discord_mc_chat/Config.java
+++ src/main/java/com/xujiayao/discord_mc_chat/Config.java
@@ -48,6 +48,7 @@ public class Config {
 
 		public boolean broadcastChatMessages = true;
 		public boolean formatChatMessages = true;
+		public boolean broadcastTellRawMessages = true;
 		public List<String> allowedMentions = List.of("everyone", "users", "roles");
 		public boolean useServerNickname = true;
 		public int discordNewlineLimit = 3;
--- src/main/java/com/xujiayao/discord_mc_chat/minecraft/mixins/MixinTellRawCommand.java
+++ src/main/java/com/xujiayao/discord_mc_chat/minecraft/mixins/MixinTellRawCommand.java
@@ -14,6 +14,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import static com.xujiayao.discord_mc_chat.Main.CONFIG;
 import static com.xujiayao.discord_mc_chat.Main.LOGGER;
 
 /**
@@ -28,6 +29,10 @@ public class MixinTellRawCommand {
 
 		if (input.startsWith("/tellraw @a ") || input.startsWith("tellraw @a ")) {
 			try {
+				if (!CONFIG.generic.broadcastTellRawMessages) {
+					return;
+				}
+
 				//#if MC >= 12105
 				MinecraftEvents.COMMAND_MESSAGE.invoker().message(ComponentArgument.getResolvedComponent(context, "message").getString(), context.getSource());
 				//#else
