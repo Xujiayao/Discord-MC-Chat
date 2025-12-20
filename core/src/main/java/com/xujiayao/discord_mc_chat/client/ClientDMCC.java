e610b37d2571eb08ea7d8bc0b08529a96585280c
Xujiayao <i@xujiayao.com>
2025-12-20 19:43:46 +0800

    Remove unused ServiceLoader import in ClientDMCC

--- core/src/main/java/com/xujiayao/discord_mc_chat/client/ClientDMCC.java
+++ core/src/main/java/com/xujiayao/discord_mc_chat/client/ClientDMCC.java
@@ -1,6 +1,5 @@
 package com.xujiayao.discord_mc_chat.client;
 
-import java.util.ServiceLoader;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
