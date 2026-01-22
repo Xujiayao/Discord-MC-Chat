b57a4bbb740a0a7b4f95dd49f03a73566fa9c078
Xujiayao <i@xujiayao.com>
2026-01-22 12:20:44 +0800

    新增Minecraft事件处理：注册服务器启动和停止事件以发送相应的数据包

--- minecraft/src/main/java/com/xujiayao/discord_mc_chat/minecraft/events/MinecraftEventHandler.java
+++ minecraft/src/main/java/com/xujiayao/discord_mc_chat/minecraft/events/MinecraftEventHandler.java
@@ -2,7 +2,11 @@ package com.xujiayao.discord_mc_chat.minecraft.events;
 
 import com.xujiayao.discord_mc_chat.DMCC;
 import com.xujiayao.discord_mc_chat.minecraft.commands.MinecraftCommands;
+import com.xujiayao.discord_mc_chat.network.NetworkManager;
+import com.xujiayao.discord_mc_chat.network.packets.MinecraftEventPacket;
 import com.xujiayao.discord_mc_chat.utils.events.EventManager;
+
+import java.util.Map;
 
 /**
  * Handles Minecraft events posted from the event manager.
@@ -15,6 +19,16 @@ public class MinecraftEventHandler {
 	 * Initializes the Minecraft event handlers.
 	 */
 	public static void init() {
+		EventManager.register(MinecraftEvents.ServerStarted.class, event -> {
+			Map<String, String> placeholders = Map.of();
+			NetworkManager.sendPacketToServer(new MinecraftEventPacket(MinecraftEventPacket.MessageType.SERVER_STARTED, placeholders));
+		});
+
+		EventManager.register(MinecraftEvents.ServerStopping.class, event -> {
+			Map<String, String> placeholders = Map.of();
+			NetworkManager.sendPacketToServer(new MinecraftEventPacket(MinecraftEventPacket.MessageType.SERVER_STOPPING, placeholders));
+		});
+
 		EventManager.register(MinecraftEvents.ServerStopped.class, event -> {
 			// Shutdown DMCC when the server is stopped
 			// Blocks until shutdown is complete
