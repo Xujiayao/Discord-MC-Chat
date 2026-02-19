61a20f52c5d6c3b8c7e3bd38a4a6c84d30401291
Xujiayao <i@xujiayao.com>
2026-02-19 22:33:23 +0800

    Add Minecraft prefix to help command output for local command senders

--- core/src/main/java/com/xujiayao/discord_mc_chat/commands/impl/HelpCommand.java
+++ core/src/main/java/com/xujiayao/discord_mc_chat/commands/impl/HelpCommand.java
@@ -3,9 +3,12 @@ package com.xujiayao.discord_mc_chat.commands.impl;
 import com.xujiayao.discord_mc_chat.commands.Command;
 import com.xujiayao.discord_mc_chat.commands.CommandManager;
 import com.xujiayao.discord_mc_chat.commands.CommandSender;
+import com.xujiayao.discord_mc_chat.commands.LocalCommandSender;
 import com.xujiayao.discord_mc_chat.utils.i18n.I18nManager;
 
 import java.util.Comparator;
+
+import static com.xujiayao.discord_mc_chat.Constants.IS_MINECRAFT_ENV;
 
 /**
  * Help command implementation.
@@ -34,11 +37,13 @@ public class HelpCommand implements Command {
 		StringBuilder builder = new StringBuilder();
 		builder.append("========== ").append(I18nManager.getDmccTranslation("commands.help.help")).append(" ==========\n");
 
+		String mcPrefix = (IS_MINECRAFT_ENV && sender instanceof LocalCommandSender) ? "dmcc " : "";
+
 		CommandManager.getCommands().stream()
 				.filter(cmd -> cmd.isVisibleTo(sender))
 				.sorted(Comparator.comparing(Command::name))
 				.forEach(cmd -> {
-					builder.append("\n").append("- ").append(cmd.name()).append(": ").append(cmd.description());
+					builder.append("\n").append("- ").append(mcPrefix).append(cmd.name()).append(": ").append(cmd.description());
 
 					for (Command.CommandArgument arg : cmd.args()) {
 						builder.append("\n    ").append("<").append(arg.name()).append(">: ").append(arg.description());
